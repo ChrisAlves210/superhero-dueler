4b8cf7374f3db11bd799895434044a7183d4d9f9
christianalves <christian.alves@students.dominican.edu>
2026-02-17 10:40:32 -0800

    created dog class

---
+++ my_dogs.py
@@ -0,0 +1,14 @@
+from dog import Dog
+
+my_dog = Dog("Rocco", "English Bulldog")
+my_dog.bark()
+
+my_other_dog = Dog("Misty", "Staffordshire Bull Terrier")
+print(my_other_dog.name)
+
+third_dog = Dog("Casey", "Boxer")
+
+# Stretch: have different dogs do different actions
+my_dog.bark()
+my_other_dog.sit()
+third_dog.roll_over()
